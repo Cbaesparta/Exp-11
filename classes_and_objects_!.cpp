4a4d0f97a9b0969148fd6a59411a74a3b5ec83b7
Sai Sankar Jena <saisankarjena4568@gmail.com>
2024-08-28 00:29:25 +0530

    Add files via upload

---
+++ classes_and_objects_!.cpp
@@ -0,0 +1,21 @@
+//SAI SANKAR JENA
+//Prn: 23070123112 
+//experiment 11: 
+
+#include <iostream>
+using namespace std;
+
+class cube
+{
+    public:
+    int height = 2;
+    int width = 3;
+    int length = 5;
+};
+
+int main()
+{
+    cube c1;
+    int vol = c1.height*c1.width*c1.length;
+    cout<<"volume:"<<vol<<endl;
+}
